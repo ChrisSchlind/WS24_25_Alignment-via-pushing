1a5a097d11e7ed422cb2c6934304c317cdc8b10d
Hannes Seibold <184473687+seha1016@users.noreply.github.com>
2024-12-17 10:46:37 +0100

    Added manual control mode for playing the game and test environment

--- src/scripts_rl/play_game.py
+++ src/scripts_rl/play_game.py
@@ -29,44 +29,80 @@ def main(cfg: DictConfig) -> None:
     camera_factory = instantiate(cfg.camera_factory, bullet_client=bullet_client, t_center=t_center)
 
     logger.info("Instantiation completed.")
+       
+    robot.home()
+    task = task_factory.create_task()
+    task.setup(env)
+    observations = [camera.get_observation() for camera in camera_factory.cameras]
 
+    # randomly select an object and area
+    id = random.randint(0, len(task.push_objects) - 1)
+    obj, area = task.get_object_and_area_with_same_id(id)
 
-    for _ in range(10):    
-        robot.home()
-        task = task_factory.create_task()
-        task.setup(env)
-        observations = [camera.get_observation() for camera in camera_factory.cameras]
-
-        # randomly select an object and area
-        id = random.randint(0, len(task.push_objects) - 1)
-        obj, area = task.get_object_and_area_with_same_id(id)
-
-        # get the object and area poses
-        obj_pose = env.get_pose(obj.unique_id)
-        area_pose = env.get_pose(area.unique_id)
+    # get the object and area poses
+    obj_pose = env.get_pose(obj.unique_id)
+    area_pose = env.get_pose(area.unique_id)
+    if cfg.auto_mode:
         print("Object pose: ", obj_pose)
-        print("Area pose: ", area_pose)
+        print("Area pose: ", area_pose) 
 
+    # Move robot to start position
+    gripper_offset = Affine(cfg.gripper_offset.translation, cfg.gripper_offset.rotation)
+    start_pose = Affine(translation=[0.35, -0.25, 0.004], rotation=[0, 0, 0, 1])
+    start_pose = start_pose * gripper_offset
+    robot.ptp(start_pose)
+
+
+    # Define Manual control
+    switch = {
+        ord("a"): Affine(translation=[-0.03, 0, 0]),
+        ord("d"): Affine(translation=[0.03, 0, 0]),
+        ord("w"): Affine(translation=[0, -0.03, 0]),
+        ord("s"): Affine(translation=[0, 0.03, 0]),
+    }
+
+    # Control settings
+    key_pressed = ord("w")
+    logger.info("Control settings:")
+    logger.info("w: move forward")
+    logger.info("s: move backward")
+    logger.info("a: move left")
+    logger.info("d: move right")
+    logger.info("q: quit")
+    logger.info("Press any key to start")
+    logger.info("Movement control is relative to the camera view displayed in the opencv window")
+
+    while key_pressed != ord("q"):
         # Display
         image_copy = copy.deepcopy(observations[0]["rgb"])
+        # Convert to rgb for visualization
+        image_copy = cv2.cvtColor(image_copy, cv2.COLOR_BGR2RGB)
         cv2.imshow("rgb", image_copy)
         depth_copy = copy.deepcopy(observations[0]["depth"])
         # rescale for visualization
         depth_copy = depth_copy / 2.0
         cv2.imshow("depth", depth_copy)
         key_pressed = cv2.waitKey(0)
-        if key_pressed == ord("q"):
-            break
 
-        # Move robot
-        gripper_offset = Affine(cfg.gripper_offset.translation, cfg.gripper_offset.rotation)
-        z_offset = Affine([0, 0, 0.05])
-        start_action = obj_pose * z_offset * gripper_offset
-        end_action = area_pose * z_offset * gripper_offset
-        robot.ptp(start_action)
-        robot.lin(end_action)
+        if cfg.auto_mode:
+            # Move robot
+            z_offset = Affine([0, 0, 0.05])
+            start_action = obj_pose * z_offset * gripper_offset
+            end_action = area_pose * z_offset * gripper_offset
+            robot.ptp(start_action)
+            robot.lin(end_action)
+        else:
+            current_pose = robot.get_eef_pose()     
+            action = switch.get(key_pressed, None)
+            if action:
+                new_pose = current_pose* action
+                logger.info(f"Moving robot to {new_pose.translation}")
+                robot.ptp(new_pose)
+                logger.info("Robot movement completed")
+                      
         
-        task.clean(env)
+    # Shut down
+    task.clean(env)
 
     with stdout_redirected():
         bullet_client.disconnect()
