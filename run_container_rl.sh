c66637c2ebe0aeca224dbc8611a4f38b7b77d6ae
LuisGbs <l.goebes1@gmail.com>
2024-12-22 12:25:16 +0100

    TEST BEFORE MERGE!!! changed Dockerfile, build_image and run_container so that WSL2 is suported

--- run_container_rl.sh
+++ run_container_rl.sh
@@ -2,11 +2,16 @@
 
 tensorflow_version="2.11.0"
 
+# Define container and host directories
 SRC_CONTAINER=/home/jovyan/workspace/src
-SRC_HOST="$(pwd)"/src
+SRC_HOST="$(pwd)/src"
 DATA_CONTAINER=/home/jovyan/data
-DATA_HOST="$(pwd)"/data
+DATA_HOST="$(pwd)/data"
 
+# Allow local connections to the X server
+xhost +local:docker
+
+# Run the Docker container
 docker run \
   --name ws2425_avp-rl \
   --privileged \
@@ -15,5 +20,12 @@ docker run \
   --net=host \
   -v "$SRC_HOST":"$SRC_CONTAINER":rw \
   -v "$DATA_HOST":"$DATA_CONTAINER":rw \
-  -e DISPLAY="$DISPLAY" \
- ws2425_avp/rl:"$tensorflow_version"
+  -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+  -e DISPLAY=$DISPLAY \
+  -e WAYLAND_DISPLAY=$WAYLAND_DISPLAY \
+  -e XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR \
+  -e PULSE_SERVER=$PULSE_SERVER \
+  ws2425_avp/rl:"$tensorflow_version"
+
+# Revoke permissions after the container stops
+xhost -local:docker
